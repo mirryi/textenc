9d1ebfbaa17ff0a3f5c7e39224b668c5cb358edd
Eric Zhao <21zhaoe@protonmail.com>
2020-11-04 17:00:52 -0500

    build: Makefile shortcuts

---
+++ Makefile
@@ -0,0 +1,30 @@
+TARGET ?= target
+
+CPPFLAGS += -std=c++17 -lstdc++
+
+RUSTC ?= rustc
+RUSTC_FLAGS := --out-dir $(TARGET)
+
+LATEXMK ?= latexmk
+
+DOCKER ?= docker
+
+.PHONY : index index-docker 32-ascii-decoder 41-utf8-decoder 42-utf8-encoder create-target
+
+index	: create-target index.tex $(wildcard sections/*) references.bib
+	$(LATEXMK)
+
+index-docker: create-target index.tex $(wildcard sections/*) references.bib
+	DOCKER_BUILDKIT=1	$(DOCKER) build --output $(TARGET) .
+
+32-ascii-decoder : sections/32-decoder.rs create-target
+	$(RUSTC) $(RUSTC_FLAGS) $<
+
+41-utf8-decoder : sections/41-utf8-parser.cpp create-target
+	$(CC) $(CPPFLAGS) -o $(TARGET)/$@ $<
+
+42-utf8-encoder : sections/42-utf8-encoder.cpp create-target
+	$(CC) $(CPPFLAGS) -o $(TARGET)/$@ $^
+
+create-target:
+	@mkdir -p $(TARGET)
--- sections/32-decoder.rs
+++ sections/32-decoder.rs
@@ -1,17 +1,28 @@
 // Pretend that the imported function from_codepoint()
 // converts decimal code point values to their string
 // representation.
-mod table;
 use table::from_codepoint;
 
-fn decode_ascii(memory: Vec<u8>) -> String {
+fn decode_ascii(memory: &Vec<u8>) -> String {
     // Convert each codepoint to its corresponding ASCII
     // character.
-    let codepoints = memory.iter().map(|n| from_codepoint(n));
+    let codepoints = memory.iter().map(|n| from_codepoint(*n));
 
     // Collect each ASCII character string to one string.
     let string: String = codepoints.collect();
 
     // Return the string.
     string
+}
+
+mod table {
+    pub fn from_codepoint(cp: u8) -> char {
+        cp as char
+    }
+}
+
+fn main() {
+    let memory = vec![72, 101, 108, 108, 111, 32, 119, 111, 114, 108, 100, 33];
+    let string = decode_ascii(&memory);
+    println!("{}", string);
 }
